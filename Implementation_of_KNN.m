917302a79b7690a46f94b7aba3bda158b4d8ee12
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2019-02-19 14:44:18 -0500

    Create Implementation_of_KNN.m

---
+++ Implementation_of_KNN.m
@@ -0,0 +1,5 @@
+% Machine Learning HW1 Problem 4
+% implement 1-NN and 3-NN classifier from 2000 points 
+% generated from two equally weighted spherical gaussians
+
+%% generate points from two equally weighted spherical gaussians
